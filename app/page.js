4cccbd5a33f3ad0971d2cafb35bd5123753a783e
Kartik Mishra <kartikmishra.km44@gmail.com>
2025-12-26 21:27:40 +0530

    on board user

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import { onBoardUser } from "@/modules/auth/actions";
 
-export default function Home() {
+export default async function Home() {
+  await onBoardUser();
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <UserButton />
