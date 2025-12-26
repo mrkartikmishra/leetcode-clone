4aa62a216c0f341a2be5670e67d134cb2bff5638
Kartik Mishra <kartikmishra.km44@gmail.com>
2025-12-26 20:20:54 +0530

    clerk auth setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <Button>click me</Button>
+      <UserButton />
     </div>
   );
 }
